68fa82b6d7e3524bbda57ffada3d3846afbc30fc
Tonlisboa <tonlisboa@hotmail.com>
2021-10-28 10:27:40 -0300

    inicio final

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void abrirToast(View view) {
+        /*
+        Toast.makeText(
+                getApplicationContext(),
+                "Ação realizada com sucesso!",
+                Toast.LENGTH_SHORT
+        ).show();
+        */
+        //Toast Customizado
+        ImageView imagem = new ImageView(getApplicationContext());
+        TextView textView = new TextView(getApplicationContext());
+        textView.setText("Olá Toast");
+        textView.setBackgroundResource(R.color.purple_500);
+
+        imagem.setImageResource(android.R.drawable.star_big_off);
+        Toast Toast = new Toast(getApplicationContext());
+        Toast.setDuration(Toast.LENGTH_LONG);
+        Toast.setView(textView);
+        Toast.show();
     }
 }
